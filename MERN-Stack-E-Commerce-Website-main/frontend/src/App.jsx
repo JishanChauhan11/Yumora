1784ac4cf3f8a15ed505adf81bc701b620e3e177
JishanChauhan11 <150168471+JishanChauhan@users.noreply.github.com>
2026-01-03 18:08:42 +0530

    Updated code

--- MERN-Stack-E-Commerce-Website-main/frontend/src/App.jsx
+++ MERN-Stack-E-Commerce-Website-main/frontend/src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import  Shop  from './Pages/shop';
-
-
-import  ShopCategory  from './Pages/ShopCategory';
-import  Product  from './Pages/Product';
-import  Cart  from './Pages/Cart';
-import  {LoginSignup}  from './Pages/LoginSignup';
+import Shop from './Pages/shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
+import men_banner from './Components/Assets/banner_mens.png'
 import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
@@ -21,16 +19,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/coffee' element={<ShopCategory category="coffee"/>}/>
-          <Route path='/womens' element={<ShopCategory banner = {women_banner} category='women' />} />
-          <Route path='/kids' element={<ShopCategory banner = {kid_banner} category='kid' />} />
+          <Route path='/coffee' element={<ShopCategory banner={men_banner} category="coffee" />} />
+          <Route path='/energy' element={<ShopCategory banner={women_banner} category="energy" />} />
+          <Route path='/hydration' element={<ShopCategory banner={kid_banner} category="hydration" />} />
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- MERN-Stack-E-Commerce-Website-main/frontend/src/Components/Hero/Hero.jsx
+++ MERN-Stack-E-Commerce-Website-main/frontend/src/Components/Hero/Hero.jsx
@@ -3,31 +3,30 @@ import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
+
 const Hero = () => {
   return (
-    <>
-            <div className="hero">
-                <div className="hero-left">
-                <h2>INSTANT FUEL</h2>
-<div>
-    <div className="hero-hand-icon">
-        <p>Twist</p>
-        {/* You can remove the hand_icon image line if you don't want the waving hand */}
-    </div>
-    <p>Shake</p>
-    <p>And Drink</p>
-</div>
-                    <div className="hero-latest-btn">
-                    <div>Shop Flavors</div>
-                        <img src={arrow_icon} alt="" />
-                    </div>
-                </div>
-                <div className="hero-right">
-                    <img src={hero_image} alt="" />
-                </div>
+    <div className='hero'>
+      <div className="hero-left">
+        <h2>THE FUTURE OF FRESHNESS</h2>
+        <div>
+            <div className="hero-hand-icon">
+                <p>new</p>
+                <img src={hand_icon} alt="" />
             </div>
-        </>
+            <p>press & shake</p>
+            <p>technology</p>
+        </div>
+        <div className="hero-latest-btn">
+            <div>View Collection</div>
+            <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
+    </div>
   )
 }
 
-export default Hero;
+export default Hero
